56d221555e063fc465b4263dab889a1dc7d08e2d
Shruti S Sajeev <shrutissajeev@gmail.com>
2025-10-26 19:39:02 +0530

    added-public-folder

--- config/passport.js
+++ config/passport.js
@@ -5,7 +5,7 @@ const pool = require("./db");
 passport.use(new GoogleStrategy({
   clientID: process.env.GOOGLE_CLIENT_ID,
   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  callbackURL: "/auth/google/callback"
+  callbackURL: "http://localhost:5500/api/auth/google/callback"
 }, async (accessToken, refreshToken, profile, done) => {
   try {
     const existingUser = await pool.query(
